add44fdbc11549f9718e6944a2358278f6234d73
blastermaster0 <60554739+blastermaster0@users.noreply.github.com>
2020-02-03 18:41:53 -0800

    Update deletePwdFile.py

--- deletePwdFile.py
+++ deletePwdFile.py
@@ -14,17 +14,16 @@ from os import environ, remove
 from xmlrpclib import ServerProxy
 
 
-host = os.environ["NZBOP_CONTROLIP"]
-port = os.environ["NZBOP_CONTROLPORT"]
-username = os.environ["NZBOP_CONTROLUSERNAME"]
-password = os.environ["NZBOP_CONTROLPASSWORD"]
-passFile = os.environ["NZBOP_UNPACKPASSFILE"]
+host = environ["NZBOP_CONTROLIP"]
+port = environ["NZBOP_CONTROLPORT"]
+username = environ["NZBOP_CONTROLUSERNAME"]
+password = environ["NZBOP_CONTROLPASSWORD"]
+passFile = environ["NZBOP_UNPACKPASSFILE"]
 
-rpcUrl = f"http://{host}:{port}@{username}:{password}/xmlrpc"
+rpcUrl = 'http://%s:%s@%s:%s/xmlrpc' % (username, password, host, port);
 
 server = ServerProxy(rpcUrl)
 listgroups = server.listgroups()
 
 if not listgroups:
     remove(passFile)
-
