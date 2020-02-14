4343b7a4c07193197d0da8d685aa8e5dfd432d39
blastermaster0 <60554739+blastermaster0@users.noreply.github.com>
2020-02-14 12:57:28 +0900

    Update deletePwdFile.py

--- deletePwdFile.py
+++ deletePwdFile.py
@@ -12,18 +12,23 @@
 
 from os import environ, remove
 from xmlrpclib import ServerProxy
+import sys
 
+# Exit codes used by NZBGet
+COMMAND_SUCCESS=93
+COMMAND_ERROR=94
 
-host = environ["NZBOP_CONTROLIP"]
-port = environ["NZBOP_CONTROLPORT"]
-username = environ["NZBOP_CONTROLUSERNAME"]
-password = environ["NZBOP_CONTROLPASSWORD"]
-passFile = environ["NZBOP_UNPACKPASSFILE"]
+host = environ.get("NZBOP_CONTROLIP")
+port = environ.get("NZBOP_CONTROLPORT")
+username = environ.get("NZBOP_CONTROLUSERNAME")
+password = environ.get("NZBOP_CONTROLPASSWORD")
+passFile = environ.get("NZBOP_UNPACKPASSFILE")
 
-rpcUrl = 'http://%s:%s@%s:%s/xmlrpc' % (username, password, host, port);
+rpcUrl = 'http://%s:%s@%s:%s/xmlrpc' % (username, password, host, port)
 
 server = ServerProxy(rpcUrl)
 listgroups = server.listgroups()
 
 if not listgroups:
     remove(passFile)
+sys.exit(COMMAND_SUCCESS)
