c8585e1d300bd1ec1d3fcdfab2f80b67c02c5ae7
Alex Gentile <agentilela@gmail.com>
2020-12-08 19:54:04 -0800

    improvements

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ import app
 import logging
 
 logger = logging.getLogger('waitress')
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
+app.logger.setLevel(logging.DEBUG)
 
 serve(app.app, host="0.0.0.0", port=7897)
